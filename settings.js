666ee5fe3aadbec12d998e897448305af8146598
madusanka1011 <109770106+madusanka1011@users.noreply.github.com>
2022-07-26 18:10:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,19 +54,19 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.vcardowner = ['94761508260'] //ur owner number
+global.ownername = "↳ 𝑅𝛩𝐿𝐿𝛯𝛸 ᴮᴼᵀ ↲ ʙʏ ɴɪᴘᴜ" //ur owner name
+global.ytname = "YT: SISCO YT" //ur yt chanel name
+global.socialm = "GitHub: MADUSANKA1011" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.owner = ['94761508260']
+global.ownertag = '94761508260' //ur tag number
+global.botname = '↳ 𝑅𝛩𝐿𝐿𝛯𝛸 ᴮᴼᵀ ↲ ʙʏ ɴɪᴘᴜ' //ur bot name
+global.linkz = "https://youtube.com/channel/UCMTPSADs0moSNNhNzLEzT-Q" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/channel/UCMTPSADs0moSNNhNzLEzT-Q" //ur website to be displayed
+global.botscript = 'https://github.com/madusanka1011' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['94761508260'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
